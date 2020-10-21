e0fcb31dff55fff4c1bb483f112e472883f9c9ee
Andrew Santos Maia <andrew.maia@talktelecom.com.br>
2020-10-21 10:40:16 -0300

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularDocker';
+export class AppComponent implements OnInit {
+  title = '[Carregando Titulo...]';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getTitle();
+  }
+
+  getTitle()  {
+    return this.http.get<any>('http://localhost:5000/teste').subscribe(
+      x => {
+        this.title = x.titulo;
+      }
+    );
+  }
 }
